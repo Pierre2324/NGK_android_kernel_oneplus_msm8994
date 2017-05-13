a0e53a637af06e65f711ab35731050ee27c80796
www.anuprita804@gmail.com <www.anuprita804@gmail.com>
2017-05-13 14:31:45 +0200

    drivers: slimbus: fix missing header and also fix build error

--- drivers/slimbus/slim-msm-ctrl.c
+++ drivers/slimbus/slim-msm-ctrl.c
@@ -23,8 +23,8 @@
 #include <linux/pm_runtime.h>
 #include <linux/of.h>
 #include <linux/of_slimbus.h>
-#include <mach/sps.h>
-#include <mach/qdsp6v2/apr.h>
+#include "../../arch/arm/mach-msm/include/mach/sps.h"
+#include "../../arch/arm/mach-msm/include/mach/qdsp6v2/apr.h"
 #include "slim-msm.h"
 
 #define MSM_SLIM_NAME	"msm_slim_ctrl"
@@ -206,8 +206,7 @@ static irqreturn_t msm_slim_interrupt(int irq, void *d)
 		 * Guarantee that interrupt clear bit write goes through before
 		 * signalling completion/exiting ISR
 		 */
-		mb();
-		msm_slim_manage_tx_msgq(dev, false, NULL);
+		
 	}
 	if (stat & MGR_INT_RX_MSG_RCVD) {
 		u32 rx_buf[10];
