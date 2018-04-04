bad92c7775f1c5845655261c79c919b7ea053034
pierre <pdbbasketball@gmail.com>
2018-04-04 19:09:35 -0400

    boeffla power && control keys

---
+++ include/linux/boeffla_powerkey_helper.h
@@ -0,0 +1,22 @@
+/*
+ * Author: andip71, 20.06.2016
+ * 
+ * Version 1.0.0
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+/*****************************************/
+// Function declarations
+/*****************************************/
+
+void boeffla_press_powerkey(void);
