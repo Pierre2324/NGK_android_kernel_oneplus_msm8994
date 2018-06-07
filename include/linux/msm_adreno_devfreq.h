60b0d97f9d36f6298349bcce63fcbef2c1176ec4
Akhil P Oommen <akhilpo@codeaurora.org>
2018-06-07 18:39:27 +0530

    [BACKPORT]: adreno_tz: Remove partner devfreq management
    Remove partner (bus) devfreq management from the adreno governor.
    Partner devfreq can be managed directly from the device driver.
    
    Change-Id: I745361c2b585e0dd91f208e09ec3001d7ecba717
    Signed-off-by: Akhil P Oommen <akhilpo@codeaurora.org>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    
    Fit 3.10.x Kernel
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- include/linux/msm_adreno_devfreq.h
+++ include/linux/msm_adreno_devfreq.h
@@ -1,12 +1,21 @@
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef MSM_ADRENO_DEVFREQ_H
 #define MSM_ADRENO_DEVFREQ_H
 
 #include <linux/devfreq.h>
 #include <linux/notifier.h>
-
-#define ADRENO_DEVFREQ_NOTIFY_SUBMIT	1
-#define ADRENO_DEVFREQ_NOTIFY_RETIRE	2
-#define ADRENO_DEVFREQ_NOTIFY_IDLE	3
 
 struct device;
 
@@ -50,12 +59,6 @@ struct devfreq_msm_adreno_tz_data {
 
 struct msm_adreno_extended_profile {
 	struct devfreq_msm_adreno_tz_data *private_data;
-	struct devfreq *bus_devfreq;
-	struct workqueue_struct *partner_wq;
-	struct work_struct partner_start_event_ws;
-	struct work_struct partner_stop_event_ws;
-	struct work_struct partner_suspend_event_ws;
-	struct work_struct partner_resume_event_ws;
 	struct devfreq_dev_profile profile;
 };
 
