50371015d8238cf2ea6787054998b0af1a282394
pierre <pdbbasketball@gmail.com>
2018-08-30 19:01:22 -0400

    Boost Devfreq device to the max for 2500 ms when userspace launches an app

--- kernel/fork.c
+++ kernel/fork.c
@@ -1645,9 +1645,10 @@ long do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost devfreq device to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
+	/* Boost Devfreq Bandwidth Device to the max for 2500 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid)){
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 2500);
+	}
 
 	/*
 	 * Do some preliminary argument and permissions checking before we
